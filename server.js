7ae49feebf0ff422b6e8d5eb28fe297906116506
zaq <zaq@clawagent.ai>
2026-03-13 00:58:59 +0900

    fix: express wildcard route syntax

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ const app = express();
 const PORT = process.env.PORT || 3760;
 
 app.use(express.static(join(__dirname, 'dist')));
-app.get('*', (req, res) => res.sendFile(join(__dirname, 'dist', 'index.html')));
+app.get('/{*path}', (req, res) => res.sendFile(join(__dirname, 'dist', 'index.html')));
 app.listen(PORT, () => console.log(`Wants app running on port ${PORT}`));
